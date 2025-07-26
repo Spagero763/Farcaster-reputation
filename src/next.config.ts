a820d549716fd6077c45785913e652aa4588ca30
afolabi ayomide <afolabiayomide870@gmail.com>
2025-07-26 01:17:09 +0000

    Thanks for sharing the logs and explanation.
    Here’s a **step-by-step in

---
+++ src/next.config.ts
@@ -0,0 +1,27 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // The cross-origin issue is specific to the dev environment and this is not needed for production.
+  // experimental: {
+  //   allowedDevOrigins: ['*'],
+  // },
+};
+
+export default nextConfig;
